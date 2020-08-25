790138dfaf52a482ff4d6319da0721ec620e5a6d
Feng Zhou <fenzhou@microsoft.com>
2020-08-25 10:26:34 +0800

    comment

--- scripts/release/debian/test_deb_package.py
+++ scripts/release/debian/test_deb_package.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
+# test in docker
 import os
 import sys
 import subprocess
